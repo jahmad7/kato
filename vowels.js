216af7896ca4bf84b723160dd644cdea46c573f3
warriorjj <jahmad095@gmail.com>
2019-03-25 20:03:46 +0000

    how to use .indexOf method to search through arrays

---
+++ vowels.js
@@ -0,0 +1,16 @@
+let numberOfVowels = function(data) {
+  // Put your solution here
+  var sum = 0;
+  var validInputs = [`a`, `e`, `i`, `o`,`u`];
+  for (var i = 0; i < data.length; i++) { 
+  	//.indexOf method looks to see if any of the valid input corelate with the value of data[i]
+  	if (validInputs.indexOf(data[i]) > -1 ) {
+  		sum++
+  	}
+  }
+  return sum;
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
